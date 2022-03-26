ddef582258ebc8d9965fbd5e3e756458e2cb3610
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-03-26 23:54:20 +0100

    Add video element in html body

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ import * as THREE from './libs/three.js-r132/build/three.module.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded!!!!');
-  console.log('Three', THREE.REVISION);
+  document.getElementById("rev").innerHTML = THREE.REVISION;
+
+  const video = document.createElement("video");
+  navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
+    video.srcObject = stream;
+    video.play();
+  });
+  video.style.position = "absolute";
+  video.style.width = 500; 
+  video.style.height = 500; 
+  document.body.appendChild(video);
 });
 
